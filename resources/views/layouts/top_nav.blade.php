45a4cdda7d293635d208fd5b9e31590dcab7fe40
Daniel Hidalgo Murillo <daniel.hidalgo.murillo@gmail.com>
2020-12-03 15:53:26 +0100

    Añadiendo topNav con Menu

---
+++ resources/views/layouts/top_nav.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar navbar-dark bg-primary" style="font-weight:bolder">
+    <a class="navbar-brand" href="#" >DanielResort</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNav" style="font-size: 1.2em">
+        <ul class="navbar-nav" style="margin: auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Quiénes Somos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Donde Estamos</a>
+            </li>
+
+        </ul>
+        <button class="btn btn-success"><i class="fas fa-angle-double-right"></i> Acceder</button>
+
+
+    </div>
+
+</nav>
