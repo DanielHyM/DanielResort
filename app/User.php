9f5adf19100cad8bfe2106fd9f6db1a0a320b316
Daniel Hidalgo Murillo <daniel.hidalgo.murillo@gmail.com>
2020-12-03 13:49:50 +0100

    test

--- app/User.php
+++ app/User.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use Notifiable;
-    // destubi
+
     /**
      * The attributes that are mass assignable.
      *
@@ -36,6 +36,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    //COMENT
 }
